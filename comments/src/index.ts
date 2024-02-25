07bcbb0ed27c907c2359f64958e07771ac9f19ae
adarsht0001 <adarsht00001@gmail.com>
2024-02-25 19:51:46 +0530

    add: comment service

---
+++ comments/src/index.ts
@@ -0,0 +1,42 @@
+import { Elysia } from "elysia";
+import { randomBytes } from "crypto";
+
+type Comments = {
+    [postId: string]: [
+        {
+            id: string;
+            content: string;
+        }
+    ];
+};
+
+const commentsByPostId: Comments = {};
+
+const app = new Elysia()
+    .get(
+        "/post/:id/comments",
+        ({ params }: { params: { id: string } }) =>
+            commentsByPostId[params.id] || []
+    )
+    .post(
+        "/post/:id/comments",
+        ({
+            params,
+            body,
+        }: {
+            params: { id: string };
+            body: { content: string };
+        }) => {
+            const commentId = randomBytes(4).toString("hex");
+            const comments = commentsByPostId[params.id] || [];
+            comments.push({ id: commentId, content: body.content });
+            commentsByPostId[params.id] = comments;
+
+            return comments;
+        }
+    )
+    .listen(3001);
+
+console.log(
+    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
