f6c41781650a54236d8849c46731777aad444731
adarsht0001 <adarsht00001@gmail.com>
2024-03-23 21:24:51 +0530

    fix: query service data sync issue solved

--- query service/src/index.ts
+++ query service/src/index.ts
@@ -2,52 +2,44 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 
 const app = new Hono();
+
 app.use("/*", cors());
 
-interface Event {
-    Type: "CommentCreated" | "PostCreated";
-    Data: {
-        ID: string;
-        Title: string;
-        Content: string;
-        PostID: string;
-    };
+const posts: Record<string, Post> = {};
+
+function eventHandler(type: EventData["Type"], data: EventData["Data"]) {
+    if (type === "PostCreated") {
+        const { ID, Title } = data;
+
+        posts[ID] = { id: ID, title: Title, comments: [] };
+    }
+
+    if (type === "CommentCreated") {
+        const { ID, Content, PostID } = data;
+
+        const post = posts[PostID];
+        post.comments.push({ id: ID, content: Content });
+    }
 }
 
-type Post = {
-    id: string;
-    title: string;
-    comments: Comment[];
-};
+(async function getAllEvents(): Promise<void> {
+    const res = await fetch("http://localhost:4000/events");
+    const data: [EventData] = await res.json();
 
-type Comment = {
-    id: string;
-    content: string;
-};
-
-const posts: Record<string, Post> = {};
+    data.forEach((event) => {
+        const { Data, Type } = event;
+        eventHandler(Type, Data);
+    });
+})();
 
 app.get("/posts", (c) => {
     return c.json(posts);
 });
 
 app.post("/events", async (c) => {
-    const body: Event = await c.req.json();
+    const body: EventData = await c.req.json();
     const { Data, Type } = body;
-    if (Type === "PostCreated") {
-        const { ID, Title } = Data;
-
-        posts[ID] = { id: ID, title: Title, comments: [] };
-    }
-
-    if (Type === "CommentCreated") {
-        const { ID, Content, PostID } = Data;
-
-        const post = posts[PostID];
-        post.comments.push({ id: ID, content: Content });
-    }
-
-    console.log(posts);
+    eventHandler(Type, Data);
 
     return c.json({});
 });
---
+++ query service/src/types.d.ts
@@ -0,0 +1,20 @@
+interface EventData {
+    Type: "CommentCreated" | "PostCreated";
+    Data: {
+        ID: string;
+        Title: string;
+        Content: string;
+        PostID: string;
+    };
+}
+
+type Comments = {
+    id: string;
+    content: string;
+};
+
+interface Post {
+    id: string;
+    title: string;
+    comments: Comments[];
+}
